ddb3a2ba1ff96b65253cc3fee2ca53d4b11fe44f
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-09 22:45:04 +0900

    Add files via upload

---
+++ Object.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Object
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            object a = 123;
+            object b = 3.1415_9265_3589_7932_3846_2643_3832_79m;
+            object c = true;
+            object d = "안녕하세요";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
---
+++ String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace String
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string a = "안녕하세요?";
+            string b = "김경훈입니다.";
+
+            char c = '안'; // 문자는 작은 따옴표로 ' '
+            string s = "안녕하세요."; // 문자열은 큰 따옴표로 " "
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
