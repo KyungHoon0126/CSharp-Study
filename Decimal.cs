03bec8c2be998292d122abbe40b59cab6b32ded1
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-09 14:04:34 +0900

    Add files via upload

---
+++ Decimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Decimal
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.1415_9265_3589_7932_3846_2643_3832_79f;   // f를 붙이면 float 형식으로 간주
+            double b = 3.1415_9265_3589_7932_3846_2643_3832_79;   // 아무것도 없으면 double
+            decimal c = 3.1415_9265_3589_7932_3846_2643_3832_79m; // m을 붙이면 decimal
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
