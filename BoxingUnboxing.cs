1e2c19a6c4604624b41c08e7e5c388d0b663d162
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-10 09:08:37 +0900

    Add files via upload

---
+++ BoxingUnboxing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BoxingUnboxing
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            object b = (object) a; // a의 담긴 값을 박싱해서 힙에 저장
+            int c = (int) b; // b에 담긴 값을 언박싱해서 스택에 저장
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            double x = 3.1414213;
+            object y = x;            //x에 담긴 값을 박싱해서 힙에 젖아
+            double z = (double) y;   // y에 담긴 값을 언박싱해서 스택에 저장
+
+            // object y = x; 부분 => object 형식에 저장할 때 형식변환 연산자를 지정하지 않으면 암시적으로 object형식으로 변환한다.
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
---
+++ IntegralConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntegralConversion
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 127;
+            Console.WriteLine(a);
+
+            int b = (int)a;
+            Console.WriteLine(b);
+
+            int x = 128;  // sbyte의 최대값 127보다 1 큰수
+            Console.WriteLine(x);
+
+            sbyte y = (sbyte)x; // 오버플로우 발생
+            Console.WriteLine(y);
+        }
+    }
+}
