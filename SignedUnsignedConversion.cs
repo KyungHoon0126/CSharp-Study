bb4a3163d1f1e90d5066d24235cf4f2bbee5d69f
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-14 10:17:56 +0900

    Add files via upload

---
+++ SignedUnsignedConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SignedUnsignedConversion
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 500;
+            Console.WriteLine(a);
+
+            uint b = (uint)a;
+            Console.WriteLine(b);
+
+            int x = -30;
+            Console.WriteLine(x);
+
+            uint y = (uint)x;      // 언더플로우
+            Console.WriteLine(y);
+        }
+    }
+}
