022197cfa5a4750116dc29131f4fe05a4fc2cebb
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-15 09:06:08 +0900

    Add files via upload

---
+++ StringNumberConverion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringNumberConversion
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            string b = a.ToString();
+            Console.WriteLine(b);
+
+            float c = 3.14f;
+            string d = c.ToString();
+            Console.WriteLine(d);
+
+            string e = "12345";
+            int f = Convert.ToInt32(e);
+            Console.WriteLine(f);
+
+            string g = "1.2345";
+            float h = float.Parse(g);
+            Console.WriteLine(h);
+        }
+    }
+}
