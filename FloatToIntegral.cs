b7c92af7c961c97848627dba8d9c19b9502a7ba4
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-17 08:20:03 +0900

    Add files via upload

---
+++ FloatToIntegral.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FloatToIntegral
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            float a = 0.9f;
+            int b = (int)a;
+            Console.WriteLine(b);
+
+            float c = 1.1f;
+            int d = (int)c;
+            Console.WriteLine(d);
+        }
+    }
+}
