a39c558fc07bbf7d151bbd24e0a64b23f9ecb8a9
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-18 09:04:29 +0900

    Add files via upload

---
+++ Constant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constant
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_INT = 2147483647;
+            const int MIN_INT = -2147483648;
+
+            Console.WriteLine(MAX_INT);
+            Console.WriteLine(MIN_INT);
+        }
+    }
+}
