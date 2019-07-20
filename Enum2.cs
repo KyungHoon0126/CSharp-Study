bb471329be41a6541f799a6d7140ce1a6d43e97f
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-20 15:05:42 +0900

    Add files via upload

---
+++ Enum2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enum2
+{
+    public class MainApp
+    {
+        private enum DialogResult { YES, NO, CANCEL, CONFIRM, OK }
+
+        private static void Main(string[] args)
+        {
+            DialogResult result = DialogResult.YES;
+
+            Console.WriteLine(result == DialogResult.YES);
+            Console.WriteLine(result == DialogResult.NO);
+            Console.WriteLine(result == DialogResult.CANCEL);
+            Console.WriteLine(result == DialogResult.CONFIRM);
+            Console.WriteLine(result == DialogResult.OK);
+        }
+    }
+}
