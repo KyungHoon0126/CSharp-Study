53b359b60974fc473dbfa83492ef2d843f2a180a
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-07-28 11:05:51 +0900

    Add files via upload

---
+++ StringSlice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace StringSlice
+{
+    public class MainApp
+    {
+        private static void Main(string[] args)
+        {
+            string greeting = "Good Morning.";
+
+            WriteLine(greeting.Substring(0, 5)); // "Good"
+            WriteLine(greeting.Substring(5)); // "morning"
+            WriteLine();
+
+            // string[] arr = greeting.Split (' ');
+
+            string[] arr = greeting.Split(new string[] { " " }, StringSplitOptions.None);
+            WriteLine("Word Count : {0}", arr.Length);
+
+            foreach (string element in arr)
+                WriteLine("{0}", element);
+        }
+    }
+}
