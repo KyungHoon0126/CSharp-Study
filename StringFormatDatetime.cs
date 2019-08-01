41d9782af0353ae8b527edbd0d7867d1bfc2bf36
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-01 13:01:09 +0900

    Add files via upload

---
+++ StringFormatDatetime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+
+namespace StringFromatDatetime
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 7, 10, 5, 32, 30);
+
+            WriteLine("12시간 형식: {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
+            WriteLine("24시간 형식: {0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
+
+            CultureInfo ciKo = new CultureInfo("ko-KR");
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciKo));
+
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", ciKo));
+            WriteLine(dt.ToString(ciKo));
+
+            CultureInfo ciEn = new CultureInfo("en-US");
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciEn));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", ciEn));
+            WriteLine(dt.ToString(ciEn));
+        }
+    }
+}
