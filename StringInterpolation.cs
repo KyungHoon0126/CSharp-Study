19f3e2ca5f260b5be2be861bd741987d8e953385
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-02 17:27:28 +0900

    Add files via upload

---
+++ StringInterpolation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace StringInteroplation
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string name = "김튼튼";
+            int age = 23;
+            WriteLine($"{name,-10}, {age:D3}");
+
+            name = "박날씬";
+            age = 30;
+            WriteLine($"{name}, {age,-10:D3}");
+
+            name = "이비실";
+            age = 17;
+            WriteLine($"{name}, {(age > 20 ? "성인" : "미성년자")}");
+        }
+    }
+}
