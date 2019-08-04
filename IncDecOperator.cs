129a152b988a99ca4a1de343a57cd31b5253d3bb
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-04 09:13:37 +0900

    Add files via upload

---
+++ IncDecOperator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IncDecOperator
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+
+            Console.WriteLine(a--);
+            Console.WriteLine(--a);
+        }
+    }
+}
