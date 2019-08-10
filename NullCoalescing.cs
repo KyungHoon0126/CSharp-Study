71b6e18a0ff73b4d1aa5eb0de14f27e39ac43e6f
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-10 13:48:21 +0900

    Add files via upload

---
+++ NullCoalescing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace NullCoalescing
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            WriteLine($"{num ?? 0}");
+
+            num = 99;
+            WriteLine($"{num ?? 0}");
+
+            string str = null;
+            WriteLine($"{str ?? "Default"}");
+
+            str = "Specific";
+            WriteLine($"{str ?? "Default"}");
+        }
+    }
+}
