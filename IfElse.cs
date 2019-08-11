5ace4b801dd8ea919b50847195f691918ce59bea
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-11 10:17:19 +0900

    Add files via upload

---
+++ IfElse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IfElse
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse( input );
+
+            if (number < 0) {
+                Console.WriteLine("음수");
+            }
+            else if (number > 0) {
+                Console.WriteLine("양수");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+
+            if(number % 2 == 0) {
+                Console.WriteLine("짝수");
+            }
+            else {
+                Console.WriteLine("홀수");
+            }
+        }
+    }
+}
