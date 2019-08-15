9d445d92c7b39b1ffffadcf44b821c3be9c52623
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-16 08:59:28 +0900

    Add files via upload

---
+++ For.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace For
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
