ffaf7165ae11bf449d495a6dcc61e19ca8561e61
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-18 20:50:53 +0900

    Add files via upload

---
+++ InfiniteFor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InfiniteFor
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            for(; ; )
+            {
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
---
+++ InfiniteWhile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InfiniteWhile
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while(true)
+            {
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
