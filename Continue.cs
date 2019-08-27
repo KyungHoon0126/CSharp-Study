dee2090c32fce529a29b4f1accb2a57369b7c4e5
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-27 20:39:34 +0900

    Add files via upload

---
+++ Continue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Continue
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"{i} : 홀수");
+            }
+        }
+    }
+}
