b59fcf0b5ee0c16a6ab20a0393a105274c6cab28
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-31 16:18:41 +0900

    Add files via upload

---
+++ Caculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Caculaotr
+{
+    public class Caculator
+    {
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    public class MainApp
+    {
+        public static void Main()
+        {
+            int result = Caculator.Plus(3, 4);
+            Console.WriteLine(result);
+
+            result = Caculator.Minus(5, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
