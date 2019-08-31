9056306b996d532ae263cb10a88b02b99974d6b7
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-31 16:18:59 +0900

    Add files via upload

---
+++ Upgrade_Caculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Upgrade_Caculator
+{
+    public class Upgrade_Caculator
+    {
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+    public class MainApp
+    {
+        public static void Main()
+        {
+            int result = Upgrade_Caculator.Plus(2, 4);
+            Console.WriteLine(result);
+
+            result = Upgrade_Caculator.Minus(6, 2);
+            Console.WriteLine(result);
+
+            result = Upgrade_Caculator.Multiply(4, 8);
+            Console.WriteLine(result);
+
+            result = Upgrade_Caculator.Divide(16, 4);
+            Console.WriteLine(result);
+        }
+    }
+}
