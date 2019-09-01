f0b7d3b217921000ba2918a7cc0d83b8f904c3b5
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-02 08:30:01 +0900

    Add files via upload

---
+++ Return.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Return
+{
+    public class MainApp
+    {
+        static int Fibonacci(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+
+        static void PrintProfile(string name, string phone)
+        {
+            if (name == "")
+            {
+                Console.WriteLine("이름을 입력해주세요.");
+                return;
+            }
+
+            Console.WriteLine($"Name:{name}, Phone:{phone}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"10번째 피보나치 수 : {Fibonacci(10)}");
+
+            PrintProfile("", "123-4567");
+            PrintProfile("김경훈", "890-1234");
+        }
+    }
+}
