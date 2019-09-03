e9258d435e57ef4208986a90280327b3802385d2
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-03 13:19:04 +0900

    Add files via upload

---
+++ SwapByValue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwapByValue
+{
+    public class MainApp
+    {
+        public static void Swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"x:{x}, y:{y}");
+
+            Swap(x, y);
+
+            Console.WriteLine($"x:{x}, y:{y}");
+        }
+    }
+}
