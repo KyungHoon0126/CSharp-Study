14fbe73686b4cda0adb25f9d5c64f60a3debeaa3
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-07 09:42:47 +0900

    Add files via upload

---
+++ SwapByRef.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwapByRef
+{
+    public class MainApp
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine($"x:{x}, y:{y}");
+
+            Swap(ref x, ref y);
+
+            Console.WriteLine($"x:{x}, y:{y}");
+        }
+    }
+}
