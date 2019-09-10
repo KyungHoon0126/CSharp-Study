82f96574a00384b99f1a38c1ad834c04a274231c
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-10 10:41:29 +0900

    Add files via upload

---
+++ UsingOut.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UsingOut
+{
+    public class MainApp
+    {
+        static void Divide(int a, int b, out int quotient, out int remainder)
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
+
+        public static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 3;
+            int c;
+            // int d;
+
+            Divide(a, b, out c, out int d);
+
+            Console.WriteLine($"a:{a}, b:{b}, a/b:{c}, a%b:{d}");
+        }
+    }
+}
