bd504017128c22715be0342ea4074c530d573e90
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-13 11:44:41 +0900

    Add files via upload

---
+++ NamedParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NamedParameter
+{
+    public class MainApp
+    {
+        static void PrintProfile(string name, string phone)
+        {
+            Console.WriteLine($"Name:{name}, Phone:{phone}");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintProfile(name: "박찬호", phone: "010-123-1234");
+            PrintProfile(phone: "010-987-9876", name: "박지성");
+            PrintProfile(name: "박세리", phone: "010-222-2222");
+            PrintProfile(name: "김경훈", phone: "010-567-5678");
+        }
+    }
+}
