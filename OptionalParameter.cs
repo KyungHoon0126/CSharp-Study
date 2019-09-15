705e19f2529bb4f2fafd3de7c5fa197aa124841c
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-15 21:58:04 +0900

    Add files via upload

---
+++ OptionalParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Optional_Parameter
+{
+    public class MainApp
+    {
+        static void PrintProfile(string name, string phone = "")
+        {
+            Console.WriteLine($"Name:{name}, Phone:{phone}");
+        }
+        
+        public static void Main(string[] args)
+        {
+            PrintProfile("태연");
+            PrintProfile("윤아", "010-123-4567");
+            PrintProfile("유리");
+            PrintProfile("서현", phone:"010-789-7890");
+        }
+    }
+}
