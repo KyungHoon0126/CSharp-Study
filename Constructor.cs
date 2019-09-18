4af7aad7f506e90ecb5e02037e97e947eb816fe2
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-19 08:28:18 +0900

    Add files via upload

---
+++ Constructor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Constructor
+{
+    public class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public Cat()
+        {
+            Name = "";
+            Color = "";
+        }
+
+        public Cat(string _Name, string _Color)
+        {
+            Name = _Name;
+            Color = _Color;
+        }
+
+        ~Cat()
+        {
+            Console.WriteLine($"{Name} : 잘가");
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야옹");
+        }
+    }
+
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat("키티", "하얀색");
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat("네로", "검은색");
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+}
