d9743a5ee651bd06c8265325439b716415acbd4a
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-21 09:43:07 +0900

    Add files via upload

---
+++ StaticField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Global
+{
+    public static int Count = 0;
+}
+
+public class ClassA
+{
+    public ClassA()
+    {
+        Global.Count++;
+    }
+}
+
+public class ClassB
+{
+    public ClassB()
+    {
+        Global.Count++;
+    }
+}
+
+public class MainApp
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Globla.Count : {Global.Count}");
+
+        new ClassA();
+        new ClassA();
+        new ClassB();
+        new ClassB();
+        
+        Console.WriteLine($"Global.Count : {Global.Count}");
+    }
+    
+}
