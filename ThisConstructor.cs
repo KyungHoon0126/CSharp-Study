d1b63673de1708187845021f35913c448d6f218e
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-09-23 08:25:08 +0900

    Add files via upload

---
+++ ThisConstructor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Constructor
+{
+    class MyClass
+    {
+        private int a, b, c;
+
+        public MyClass()
+        {
+            this.a = 5425;
+            Console.WriteLine("MyClass()");
+        }
+
+        public MyClass(int b) : this()
+        {
+            this.b = b;
+            Console.WriteLine($"MhyClass{b}");
+        }
+
+        public MyClass(int b, int c) : this(b)
+        {
+            this.c = c;
+            Console.WriteLine($"MyClass{b}, {c}");
+        }
+
+        public void PrintFields()
+        {
+            Console.WriteLine($"a:{a}, b:{b}, c:{c}");
+        }
+    }
+
+    class MainApp
+    {
+        public static void Main(string[] args)
+        {
+            MyClass a = new MyClass();
+            a.PrintFields();
+            Console.WriteLine();
+
+            MyClass b = new MyClass();
+            b.PrintFields();
+            Console.WriteLine();
+
+            MyClass c = new MyClass();
+            c.PrintFields();
+            Console.WriteLine();
+
+
+        }
+    }
+}
