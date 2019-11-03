647e6ad95ac359027fd18cdd05594257c23db104
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-11-03 22:46:24 +0900

    Add files via upload

---
+++ Overriding.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Overriding
+{
+    class ArmorSuite
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("Armored");
+        }
+    }
+
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Repulsor Rays Armed");
+        }
+    }
+
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Double-Barrel Cannons Armed");
+            Console.WriteLine("Micro-Rocket Launcher Armed");
+        }
+    }
+
+    class Overriding
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating ArmorSuite...");
+
+            ArmorSuite armorSuite = new ArmorSuite();
+            armorSuite.Initialize();
+
+            Console.WriteLine("\nCreating IronMan...");
+            ArmorSuite ironman = new IronMan();
+            ironman.Initialize();
+
+            Console.WriteLine("\nCreating WarMachine...");
+            ArmorSuite warmachine = new WarMachine();
+            warmachine.Initialize();
+        }
+    }
+}
