794672b27448c680b7097b12ab275984d8c661d3
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-11-07 22:32:24 +0900

    Add files via upload

---
+++ SealedMethod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_SealedMethod
+{
+    class Base
+    {
+        public virtual void SealMe()
+        {
+
+        }
+    }
+
+    class Derived : Base
+    {
+        // sealed 키워드를 이용하여 virtual로 선언된 가상 메소드를 오버라이딩한 버전의 메소드만 봉인이 가능하다.
+        public sealed override void SealMe()
+        {
+            
+        }
+    }
+
+    class WantToOverride : Derived
+    {
+        public override void SealMe()
+        {
+
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
