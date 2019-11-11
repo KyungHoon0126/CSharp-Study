0d0fcc4b016e92f111b8bec5176ef88caed61e5b
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-11-11 21:59:40 +0900

    Add files via upload

---
+++ NestedClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_NestedClass
+{
+    class Configuration
+    {
+        List<ItemValue> listConfig = new List<ItemValue>();
+
+        public void SetConfig(string item, string value)
+        {
+            ItemValue iv = new ItemValue();
+            iv.SetValue(this, item, value);
+        }
+
+        public string GetConfig(string item)
+        {
+            foreach (ItemValue iv in listConfig)
+            {
+                if (iv.GetItem() == item)
+                {
+                    return iv.GetValue();
+                }
+            }
+
+            return "";
+        }
+        
+
+        // Configuration 클래스 안에 선언된 중첩 클래스로, private이기 때문에 Configuration 클래스 밖에서는 보이지 않는다.
+        private class ItemValue
+        {
+            private string item;
+            private string value;
+
+            public void SetValue(Configuration config, string item, string value)
+            {
+                this.item = item;
+                this.value = value;
+
+                bool found = false;
+
+                for(int i = 0; i < config.listConfig.Count; i++)
+                {
+                    if(config.listConfig[i].item == item)
+                    {
+                        config.listConfig[i] = this;
+                        found = true;
+                        break; 
+                    }
+                }
+
+                if (found == false)
+                {
+                    config.listConfig.Add(this);
+                }
+            }
+
+            public string GetItem()
+            {
+                return item;
+            }
+            public string GetValue()
+            {
+                return value;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Configuration config = new Configuration();
+            config.SetConfig("Version", "V 5.0");
+            config.SetConfig("Size", "655,324 KB");
+
+            Console.WriteLine(config.GetConfig("Version"));
+            Console.WriteLine(config.GetConfig("Size"));
+
+            config.SetConfig("Version", "V 5.0.1");
+            Console.WriteLine(config.GetConfig("Version"));
+        }
+    }
+}
