46b81d5b0a45ea0562dfc923f6e91d4e5f7ea4c4
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-11-14 22:34:20 +0900

    Add files via upload

---
+++ Structure.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Structure
+{
+    class Program
+    {
+        struct Point3D
+        {
+            public int X;
+            public int Y;
+            public int Z;
+
+            public Point3D(int X, int Y, int Z)
+            {
+                this.X = X;
+                this.Y = Y;
+                this.Z = Z;
+            }
+
+            public override string ToString()
+            {
+                return string.Format($"{X}, {Y}, {Z}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Point3D p3d1; // 선언 자체로 인스턴스 생성.
+
+            p3d1.X = 10;
+            p3d1.Y = 20;
+            p3d1.Z = 40;
+
+            Console.WriteLine(p3d1.ToString());
+
+            Point3D p3d2 = new Point3D(100, 200, 300); // 물론 생성자를 이용한 인스턴스 생성도 가능하다.
+            Point3D p3d3 = p3d2; // 구조체의 인스턴스를 다른 인스턴스에 할당하면 깊은 복사가 이루어진다.
+            p3d3.Z = 400;
+
+            Console.WriteLine(p3d2.ToString());
+            Console.WriteLine(p3d3.ToString());
+        }
+    }
+}
