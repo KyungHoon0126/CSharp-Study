434c9559fb8454ce4f16992330791d22689c6d4f
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-11-24 09:38:18 +0900

    Add files via upload

---
+++ OldStack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OldStack
+{
+    public class OldStack
+    {
+        object[] _objList;
+        int _pos;
+
+        public OldStack(int size)
+        {
+            _objList = new object[size];
+        }
+
+        public void Push(object newValue)
+        {
+            _objList[_pos] = newValue;
+            _pos++;
+        }
+
+        public object Pop()
+        {
+            _pos--;
+            return _objList[_pos];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OldStack stack = new OldStack(10);
+            stack.Push(1); // 박싱 발생
+            stack.Push(2); // 박싱 발생
+            stack.Push(3); // 박싱 발생
+
+            Console.WriteLine(stack.Pop()); // 언박싱 발생
+            Console.WriteLine(stack.Pop()); // 언박싱 발생
+            Console.WriteLine(stack.Pop()); // 언박싱 발생
+        }
+    }
+}
