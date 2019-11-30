dfbaed7ae1ebaddba4df0ede99b2acafece37418
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-11-30 15:24:56 +0900

    Add files via upload

---
+++ GenericClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_GenericClass
+{
+    // 형식 매개변수의 이름은 임의로 줄 수 있다.
+    public class GenericSample<Type>
+    {
+        Type _item;
+
+        public GenericSample(Type value)
+        {
+            _item = value;
+        }
+    }
+
+    // 2개 이상 지정하는 것도 가능하다.
+    public class TwoGeneric<K, V>
+    {
+        K _key;
+        V _value;
+
+        public void Set(K key, V value)
+        {
+            _key = key;
+            _value = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
