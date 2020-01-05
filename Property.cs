a7fbce5bd23f2f3e1f0f27de674ce091de1cb109
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-01-05 22:05:48 +0900

    add : CSharp_Property

---
+++ Property.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Property
+{
+    class Circle
+    {
+        double pi = 3.14;
+
+        // 프로퍼티 이용
+        public double Pi
+        {
+            get { return pi; }
+            set { pi = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            c.Pi = 3.141592; // set으로 사용
+            double piValue = c.Pi; // get으로 사용
+        }
+    }
+}
