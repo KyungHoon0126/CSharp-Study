2dfd5cd6bf89074ce68503f9b05c3c8be9a678b1
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-01-14 15:25:25 +0900

    add : CSharp_Equals

---
+++ CSharp_Equals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Equals
+{
+    class Book
+    {
+        decimal _isbn;
+
+        public Book(decimal isbn)
+        {
+            _isbn = isbn;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book(9788998139018);
+            Book book2 = new Book(9788998139018);
+
+            Console.WriteLine(book1.Equals(book2));
+
+            // False
+
+            string txt1 = new string(new char[] { 't', 'e', 'x', 't' });
+            string txt2 = new string(new char[] { 't', 'e', 'x', 't' });
+
+            Console.WriteLine(txt1.Equals(txt2));
+
+            // True
+        }
+    }
+}
