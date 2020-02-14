2ccf1ee8ecea5af3260661e0cc3d24c21f70133c
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-02-14 19:58:04 +0900

    [ADD] : Method_Overload

---
+++ Method_Overload.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Method_Overload
+{
+    // 오버로가 없다면 유사한 메서드를 여러 개 정의 해야 한다.
+
+    //class Mathematics
+    //{
+    //    public int AbsInt(int value)
+    //    {
+    //        return (value >= 0) ? value : -value;
+    //    }
+
+    //    public double AbsDouble(double value)
+    //    {
+    //        return (value >= 0) ? value : -value;
+    //    }
+
+    //    public decimal AbsDecimal(decimal value)
+    //    {
+    //        return (value >= 0) ? value : -value;
+    //    }
+    //}
+
+    // 오버로드는 동일한 이름의 메서드로 일관성 있게 클래스 작성 가능.
+    class Mathematics
+    {
+        public int Abs(int value)
+        {
+            return (value >= 0) ? value : -value;
+        }
+
+        public double Abs(double value)
+        {
+            return (value >= 0) ? value : -value;
+        }
+
+        public decimal Abs(decimal value)
+        {
+            return (value >= 0) ? value : -value;
+        }
+    }
+
+    class Method_Overload
+    {
+        static void Main(string[] args)
+        {
+            Mathematics math = new Mathematics();
+            Console.WriteLine(math.Abs(-5));
+            Console.WriteLine(math.Abs(-10.052));
+            Console.WriteLine(math.Abs(20.01m));
+        }
+    }
+}
+
