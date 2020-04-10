1c00ec2686eece6e3e84b8bfad43ab1540bbacbe
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-04-10 21:11:22 +0900

    [ADD] : OpenCv_Video

---
+++ OpenCv_Video.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace OpenCV_Pratice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoCapture video = new VideoCapture(0);
+            Mat frame = new Mat();
+
+            while (Cv2.WaitKey(33) != 'q')
+            {
+                video.Read(frame);
+                Cv2.ImShow("KyungHoon", frame);
+            }
+
+            frame.Dispose();
+            video.Release();
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
