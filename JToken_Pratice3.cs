5d7a196d231392ee278a31ce76e15588b5b3765e
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-05-08 20:37:27 +0900

    [ADD] : JToken_Pratice using JsonConvert.DeserializeObject

---
+++ JToken_Pratice3.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace JToken_Pratice3
+{
+    class JToken_Pratice3
+    {
+        static void Main()
+        {
+            using (StreamReader r = new StreamReader("TestJson.json"))
+            {
+                string json = r.ReadToEnd();
+                ReadJson(json);
+            }
+        }
+
+        static void ReadJson(string jsonStr)
+        {
+            JObject jObject = JObject.Parse(jsonStr);
+            TestJson testJson = JsonConvert.DeserializeObject<TestJson>(jObject["TestJson"].ToString());
+
+            Debug.WriteLine(testJson.DataStr);
+            Debug.WriteLine(testJson.DataInt);
+            foreach(var data in testJson.DataArray)
+            {
+                Debug.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ TestJson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JToken_Pratice3
+{
+    class TestJson
+    {
+        public string DataStr;
+        public int DataInt;
+        public List<string> DataArray;
+    }
+}
