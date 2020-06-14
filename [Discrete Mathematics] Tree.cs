7e374e19fa04753c82f755cb392ee6e79fa9990a
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-06-14 20:53:02 +0900

    [ADD] : Discrete Mathmatics Tree

---
+++ [Discrete Mathematics] Tree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Math_Tree
+{
+    public class Program
+    {
+        public static int[] treeNum = new int[100];
+        public const int LIMIT = 10;
+
+        public static void Main()
+        {
+            for (int i = 1; i < LIMIT; i++)
+            {
+                int j = 1;
+
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                while (treeNum[j] != 0)
+                {
+                    if(treeNum[j] > num)
+                    {
+                        j *= 2;
+                    }
+                    else
+                    {
+                        j = j * 2 + 1;
+                    }
+                }
+
+                treeNum[j] = num;
+
+
+                Console.WriteLine(j + " : " + treeNum[j]);
+            }
+        }
+    }
+}
