cbc63769561772fefc4339e678f290aa6dd0e391
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-06-15 20:51:08 +0900

    [ADD] : AsyncAwait Study

---
+++ AsyncAwaitDescription.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class AsyncAwaitDescription
+{
+    static void Main()
+    {
+        Task.Run(() => DoPrint());
+        Console.WriteLine("async await 사용 예제");
+        Thread.Sleep(1);
+    }
+
+    static async void DoPrint()
+    {
+        await PrintNumberAsync();
+    }
+
+    static async Task PrintNumberAsync()
+    {
+        await Task.Run(() => {
+            for(int i = 0; i < 300; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+        });
+    }
+}
