0faeb1759190a01adfe16ef1c5dde03716662f60
KyungHoon <kidi052812@gmail.com>
2020-08-19 20:45:15 +0900

    [ADD] : This

--- This.cs
+++ This.cs
@@ -1,46 +1,124 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace This
+namespace CSharp_this
 {
-    internal class Employee
+    #region this_Ex_1
+    //class Book
+    //{
+    //    decimal _isbn;
+    //    public decimal ISBN
+    //    {
+    //        get { return this._isbn; }
+    //    }
+
+    //    public Book(decimal isbn)
+    //    {
+    //        this._isbn = isbn;
+    //    }
+
+    //    public decimal GetISBN()
+    //    {
+    //        return this.ISBN;
+    //    }
+
+    //    public void Sell()
+    //    {
+    //        Console.WriteLine("Sell : " + this.GetISBN());
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Book book = new Book(978126548);
+    //        book.Sell();
+    //    }
+    //}
+    #endregion
+
+    #region this_Ex_2
+    //class Book
+    //{
+    //    public string title;
+    //    public decimal isbn13;
+    //    public string author;
+
+    //    public Book(string title) : this(title, 0)
+    //    {
+
+    //    }
+
+    //    public Book(string title, decimal isbn13) : this(title, isbn13, string.Empty)
+    //    {
+
+    //    }
+
+    //    // 초기화 코드를 하나의 생성자에서 처리
+    //    public Book(string title, decimal isbn13, string author)
+    //    {
+    //        this.title = title;
+    //        this.isbn13 = isbn13;
+    //        this.author = author;
+    //    }
+
+    //    public Book() : this(string.Empty, 0, string.Empty)
+    //    {
+
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Book book = new Book("김경훈", 5456238, "김경훈");
+    //        Console.WriteLine(book.title + " " + book.isbn13 + " " + book.author);
+    //    }
+    //}
+    #endregion
+
+    #region this_Ex_3
+    class Book
     {
-        private string Name;
-        private string Position;
+        string title; // 인스턴스 필드
+        static int count; // 정적 필드
 
-        public void SetName(string Name)
+        public Book(string title) // 인스턴스 생성자
         {
-            this.Name = Name;
+            this.title = title; // this로 인스턴스 필드 식별 가능
+            this.Open(); // this로 인스턴스 메서드 식별 가능
+            Increment(); // 정적 메소드 사용 가능
         }
 
-        public string GetName()
+        void Open()
         {
-            return Name;
+            Console.WriteLine(this.title); // 인스턴스 멤버 사용 가능
+            Console.WriteLine(count); // 정적 멤버 사용 가능
         }
 
-        public void SetPosition(string Position)
+        public void Close()
         {
-            this.Position = Position;
+            Console.WriteLine(this.title + " 책을 덮는다.");
         }
 
-        public string GetPosition()
+        static void Increment() // 정적 메서드
         {
-            return this.Position;
+            count++; // 정적 필드 사용 가능
+                     // 정적 메서드에는 this가 없으므로 인스턴스 멤버 사용 불가능
+        }
+
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                Book book = new Book("김경훈");
+                book.Close();
+            }
         }
     }
-
-    internal class MainApp
-    {
-        private static void Main(string[] args)
-        {
-            Employee pooh = new Employee();
-            pooh.SetName("Pooh");
-            pooh.SetPosition("Waiter");
-            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
-
-            Employee tigger = new Employee();
-            tigger.SetName("Tigger");
-            tigger.SetPosition("Cleaner");
-            Console.WriteLine($"{tigger.GetName()} {tigger.GetPosition()}");
-        }
-    }
+    #endregion
 }
